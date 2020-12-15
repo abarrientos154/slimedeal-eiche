49a7761ccb3e268311f15f5355c544c53dfe4738
Andres Barrientos <abarrientos.wx@gmail.com>
2020-12-15 19:05:41 -0400

    fix

--- api/app/Functions/Email.js
+++ api/app/Functions/Email.js
@@ -3,6 +3,7 @@ const Mail = use('Mail')
 exports.sendMail = async (to, subject, message, attach, cc, bcc) => {
   try {
     await Mail.raw(message, (msg) => {
+      console.log(msg,'test')
       msg.from('revisiones@slimedeal.com', 'Slimedeal')
       msg.to(to)
       msg.subject(subject)
